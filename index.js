6f6fd8dbdd8106c58592c41dce17cde5b75f05e7
Matt McLaughlin <mamclaughlin87@gmail.com>
2018-02-06 23:50:42 -0600

    pushing style updates and pagination function

--- index.js
+++ index.js
@@ -45,17 +45,43 @@ function getShapeList(dataForTable){
     return allShapes.filter(getUnique).sort();
 };
 
+//initialize pagination variables
+var curPage = 0;
+var firstResult = 0;
+var lastResult = 50;
+var lastPage = Math.floor(dataSet.length/50)
+var paginationList = d3.select('.pagination');
+paginationList.on('click', paginationClick);
+var firstPage = d3.select('#firstPage');
+var prevPage = d3.select('#prevPage');
+var thisPage = d3.select('#thisPage');
+var nextPage = d3.select('#nextPage');
+var lastPage = d3.select('#lastPage');
+var firstLi = d3.select('#firstLi');
+var prevLi = d3.select('#prevLi');
+var thisLi = d3.select('#thisLi');
+var nextLi = d3.select('#nextLi');
+var lastLi = d3.select('#lastLi');
 
 //render initial table
 var table = d3.select('#dataTable');
 var dataColumns = ["datetime","city","state","country","shape","durationMinutes","comments"]
 
 function renderTable(dataForTable) {
-    for(i=0; i<dataForTable.length; i++){
-        table.append('tr')
+    firstResult = curPage*50
+    if (dataForTable.length - firstResult > 50){
+        lastResult = 50;
+    }
+    else {
+        lastResult = (dataForTable.length - firstResult) - 1;
+    };
+    console.log("last result", lastResult)
+    for(i=0; i<lastResult; i++){
+        var newRow = table.append('tr')
+        newRow.append('td').attr('style', 'word-wrap: break-word;min-width: 50px;max-width: 400px;').text(firstResult + i + 1);
         for(j=0; j<dataColumns.length; j++){
-            var dataPoint = dataForTable[i][dataColumns[j]];
-            table.append('td').attr('style', 'word-wrap: break-word;min-width: 50px;max-width: 400px;').text(dataPoint);
+            var dataPoint = dataForTable[firstResult+i][dataColumns[j]];
+            newRow.append('td').attr('style', 'word-wrap: break-word;min-width: 50px;max-width: 400px;').text(dataPoint);
         };
     };
     createDropdown(countrySelector, getCountryList,dataForTable);
@@ -64,6 +90,9 @@ function renderTable(dataForTable) {
     countrySelector.property('value', selectedCountry);
     stateSelector.property('value', selectedState);
     shapeSelector.property('value', selectedShape);
+    lastPage = Math.floor(dataForTable.length/50)
+    console.log(lastPage)
+    renderPagination();
 };
 
 renderTable(dataSet);
@@ -89,16 +118,8 @@ function dateClick(){
     clearTable();
     clearDropdowns();
     var selectedDate = dateInput.node().value.trim();
-    // if (selectedDate == ""){
-    //     dataForTable = dataSet;
-    // }
-    // else {
-    //     dataForTable = dataForTable.filter(function(dataForTable){
-    //         return dataForTable.datetime == selectedDate;
-    //         ////ADD EVERYTHING IN HERE SET WHAT THE "SELECTED" ITEMS ARE OUTSIDE 
-    //     });
-    // };
     setFilters(selectedDate,"datetime");
+    curPage = 0;
     renderTable(dataForTable);
 };
 
@@ -112,6 +133,7 @@ function cityClick(){
     clearDropdowns();
     var selectedCity = cityInput.node().value.trim().toLowerCase();
     setFilters(selectedCity,"city");
+    curPage = 0;
     renderTable(dataForTable);
 };
 
@@ -123,6 +145,7 @@ function countryChange(){
     selectedCountry = countrySelector.node().value
     clearDropdowns();
     setFilters(selectedCountry,"country");
+    curPage = 0;
     renderTable(dataForTable);
 };
 
@@ -134,6 +157,7 @@ function stateChange(){
     selectedState = stateSelector.node().value
     clearDropdowns();
     setFilters(selectedState,"state");
+    curPage = 0;
     renderTable(dataForTable);
 };
 
@@ -145,6 +169,7 @@ function shapeChange(){
     selectedShape = shapeSelector.node().value
     clearDropdowns();
     setFilters(selectedShape,"shape");
+    curPage = 0;
     renderTable(dataForTable);
 };
 
@@ -173,6 +198,59 @@ function clearAll(){
     cityInput.property('value', selectedShape);
     clearTable();
     clearDropdowns();
-    
+    curPage = 0;
     renderTable(dataForTable);
+};
+
+//handle pagination
+function paginationClick(){
+    // d3.event.preventDefault();
+    var paginationBtn = d3.event.target;
+    console.log(paginationBtn.text);
+    if (paginationBtn.id == 'prevPage' || paginationBtn.id == 'thisPage' || paginationBtn.id == 'nextPage'){
+        curPage = (paginationBtn.text - 1);
+    }
+    else if (paginationBtn.id == 'firstPage' || paginationBtn.text == "&laquo;&laquo;"){
+        curPage = 0;
+    }
+    else if (paginationBtn.id == 'lastPage' || paginationBtn.text == "&raquo;&raquo;"){
+        curPage = Math.floor(dataForTable.length/50);
+    };
+    console.log(curPage);
+    clearTable();
+    clearDropdowns();
+    renderTable(dataForTable);
+};
+
+function renderPagination() {
+    if (curPage == 0){
+        prevPage.text("1")
+        thisPage.text("2");
+        nextPage.text("3");
+        firstLi.classed("disabled active", true);
+        prevLi.classed("active", true);
+        thisLi.classed("active", false);
+        nextLi.classed("active", false);
+        lastLi.classed("disabled active", false);
+    }
+    else if(curPage == lastPage){
+        prevPage.text(`${lastPage-2}`)
+        thisPage.text(`${lastPage-1}`);
+        nextPage.text(`${lastPage}`);
+        firstLi.classed("disabled active", false);
+        prevLi.classed("active", false);
+        thisLi.classed("active", false);
+        nextLi.classed("active", true);
+        lastLi.classed("disabled active", true);
+    }
+    else {
+        prevPage.text(`${curPage}`)
+        thisPage.text(`${curPage+1}`);
+        nextPage.text(`${curPage+2}`);
+        firstLi.classed("disabled active", false);
+        prevLi.classed("active", false);
+        thisLi.classed("active", true);
+        nextLi.classed("active", false);
+        lastLi.classed("disabled active", false);
+    };
 };
